b45cf5294eb86f323d6678b1255e5c22da7376be
Diana Utan <dia_utzi@yahoo.com>
2019-02-04 18:39:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,31 +1,32 @@
-function forLoop(array) {
-  for( var i=0; i<25; i++) {
-    if ( i === 1 ) {
-      array.push( `I am 1 strange loop.`)
+function forLoop(arr) {
+  for( let i =0; i< 25; i++) {
+    if (i === 1) {
+      arr.push("I am 1 strange loop.");
     }
     else {
-      array.push( `I am ${i} strange loops.`)
+      arr.push(`I am ${i} strange loops.`);
     }
   }
-   return array;
+  return arr;
 }
 
 function whileLoop(num) {
-  while ( num > 0) {
-    console.log( --num)
+  while(num > 0) {
+    console.log(num);
+     num--;
   }
-  return "done"
-    
+  return "done";
 }
 
-function doWhileLoop(array) {
-  var i=0; 
+function doWhileLoop(num) {
+  var i=0;
   function incrementVariable() {
-  i = i + 1;
-}
+    i = i+1;
+    return i;
+  }
   do {
-    array.pop()
-  } while ( array.length > 0 && incrementVariable() );
-  return array;
-  
-}
+    console.log("I ran once regardless.") 
+    
+  }
+    while (incrementVariable() < num);
+  }
